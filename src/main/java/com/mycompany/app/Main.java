c51ed99b8f6ea0313803a49af583abece925bf6c
Dulini Gunasekara <50261564+Duli96@users.noreply.github.com>
2025-01-11 14:21:02 +0000

    Change Main class for book persistance

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+import com.mycompany.app.entities.Book;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -9,5 +11,17 @@ public class Main {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_persistence_unit");
 
     EntityManager em = emf.createEntityManager();
+    try {
+      em.getTransaction().begin();
+      Book book = new Book();
+      book.setName("first book");
+      book.setIsbn("1234");
+
+      em.persist(book);
+      em.getTransaction().commit();
+    } finally {
+      em.close();
+    }
+
   }
 }
